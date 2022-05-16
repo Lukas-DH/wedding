1356e302a09e54d7d9baef494a49ebb216fd207f
Lukas <lukas@open@ivf.com>
2022-05-16 15:55:14 +0200

    next config and static page package.json

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      "/": { page: "/" },
+      "/email": { page: "/email" },
+      "/email/index": { page: "/email" },
+      "/error": { page: "/error" },
+      "/error/index": { page: "/error" },
+      "/thanks": { page: "/thanks" },
+      "/thanks/index": { page: "/thanks" },
+    };
+  },
 };
-
-module.exports = nextConfig;
